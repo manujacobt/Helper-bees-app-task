eb01664f4230995a8949c79cf142ca1c9ad1a15d
manu <manjacobt@gmail.com>
2025-03-15 10:23:07 +0530

    changes-3

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -9,15 +9,34 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class QuestionnaireComponent {
   questionnaireForm: FormGroup;
+  currentStep = 1;
+  totalSteps = 5;
+  progressValue = (this.currentStep / this.totalSteps) * 100;
+
   genres: string[] = [
     'Action', 'Horror', 'Drama', 'Thriller', 'Science Fiction', 'Fantasy',
     'Western', 'Romantic', 'Comedy', 'Kevin Hart Buddy Comedy', 'Noir', 'None of the above'
   ];
-  progressValue = 20; // Initial progress value, change it as needed
+  experiences: string[] = ['0-3 years', '4-6 years', '7 or more years'];
+
+  movieSnacks: string[] = ['Popcorn', 'Junior Mints', 'Skittles', 'Nachos', 'Milk Duds',
+    'I only watch Criterion Collection films at Arthouse theaters that disallow snacks because there might be a crinkling sound that disrupts other patrons.'
+  ];
 
   constructor(private fb: FormBuilder) {
     this.questionnaireForm = this.fb.group({
-      selectedGenres: this.fb.array([])
+      selectedGenres: this.fb.array([]),
+      experience: [''],
+      favoriteSnack: [''],
+      favoriteMovies: this.fb.array([]),
+      selectedFeatures: this.fb.array([]),
+      address: this.fb.group({
+        address1: [''],
+        address2: [''],
+        city: [''],
+        state: [''],
+        zipCode: ['']
+      })
     });
   }
 
@@ -25,20 +44,55 @@ export class QuestionnaireComponent {
     return this.questionnaireForm.get('selectedGenres') as FormArray;
   }
 
-  onCheckboxChange(event: any) {
-    const selectedGenres = this.selectedGenres;
+  get selectedFeatures(): FormArray {
+    return this.questionnaireForm.get('selectedFeatures') as FormArray;
+  }
 
+  get favoriteMovies(): FormArray {
+    return this.questionnaireForm.get('favoriteMovies') as FormArray;
+  }
+
+  addFavoriteMovie() {
+    this.favoriteMovies.push(this.fb.group({
+      title: [''],
+      year: ['']
+    }));
+  }
+
+  removeFavoriteMovie(index: number) {
+    (this.favoriteMovies as FormArray).removeAt(index);
+  }
+
+  onCheckboxChange(event: any, formArray: FormArray) {
     if (event.target.checked) {
-      selectedGenres.push(this.fb.control(event.target.value));
+      formArray.push(this.fb.control(event.target.value));
     } else {
-      const index = selectedGenres.controls.findIndex(x => x.value === event.target.value);
-      selectedGenres.removeAt(index);
+      const index = formArray.controls.findIndex(x => x.value === event.target.value);
+      formArray.removeAt(index);
     }
   }
 
+  onRadioChange(controlName: string, value: string) {
+    this.questionnaireForm.patchValue({ [controlName]: value });
+  }
+
   onNext() {
+    if (this.currentStep < this.totalSteps) {
+      this.currentStep++;
+      this.updateProgress();
+    }
     console.log(this.questionnaireForm.value);
-    // Increase the progressValue as you navigate through the questions
-    this.progressValue += 20; 
+  }
+
+  onPrevious() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+      this.updateProgress();
+    }
+  }
+
+  updateProgress() {
+    this.progressValue = (this.currentStep / this.totalSteps) * 100;
   }
 }
+
